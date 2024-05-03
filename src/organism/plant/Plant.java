dd19bd9305144ad5febf5450c9a315da3307f989
MRaflyH <raflyhendrakusuma@gmail.com>
2024-05-03 14:30:21 +0700

    Update Plant.java

--- src/organism/plant/Plant.java
+++ src/organism/plant/Plant.java
@@ -3,5 +3,33 @@ package organism.plant;
 import organism.Organism;
 
 public abstract class Plant extends Organism {
+    private Integer cost;
+    private Integer range;
+    private Integer cooldown;
+    private Integer currentCooldown;
+
+    public Plant(String name, Integer health, Integer attackDamage, Integer attackSpeed, Boolean isAquatic, Integer cost, Integer range, Integer cooldown) {
+        super(name, health, attackDamage, attackSpeed, isAquatic);
+        this.cost = cost;
+        this.range = range;
+        this.cooldown = cooldown;
+        currentCooldown = cooldown;
+    }
+
+    public Integer getCost() {
+        return cost;
+    }
+
+    public Integer getRange() {
+        return range;
+    }
+
+    public Integer getCooldown() {
+        return cooldown;
+    }
+
+    public Integer getCurrentCooldown() {
+        return currentCooldown;
+    }
 
 }
