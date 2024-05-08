fb63e71e2dd9862d28b5a2c85080a9b611fd4291
MRaflyH <raflyhendrakusuma@gmail.com>
2024-05-08 09:24:25 +0700

    Update Lilypad.java

--- src/organism/plant/Lilypad.java
+++ src/organism/plant/Lilypad.java
@@ -8,12 +8,43 @@ public class Lilypad extends Plant {
         super("Lilypad", 100, 0, 0, true, 25, 0, 10);
     }
 
+    public Integer getRange() {
+        if (hasPlant() == true) {
+            return plant.getRange();
+        }
+        else {
+            return super.getRange();
+        }
+    }
+
+    public Integer getCooldown() {
+        if (hasPlant() == true) {
+            return plant.getCooldown();
+        }
+        else {
+            return super.getCooldown();
+        }
+    }
+
+    public Integer getCurrentCooldown() {
+        if (hasPlant() == true) {
+            return plant.getCurrentCooldown();
+        }
+        else {
+            return super.getCurrentCooldown();
+        }
+    }
+
     public Plant getPlant() {
         return plant;
     }
 
     public void setPlant(Plant plant) {
         this.plant = plant;
+    }
+
+    public Boolean hasPlant() {
+        return plant != null;
     }
 
 }
