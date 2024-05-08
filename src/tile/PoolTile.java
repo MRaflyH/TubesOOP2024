f9e73f4e4dec777b21603a329c48517127f41c3c
MRaflyH <raflyhendrakusuma@gmail.com>
2024-05-08 12:11:29 +0700

    Update PoolTile.java

--- src/tile/PoolTile.java
+++ src/tile/PoolTile.java
@@ -1,9 +1,30 @@
 package tile;
 
+import organism.plant.*;
+
 public class PoolTile extends Tile {
     
     public PoolTile() {
         super("Pool Tile");
     }
 
+    public void addPlant(Plant plant) {
+        if (!hasPlant()) {
+            if (plant.getIsAquatic()) {
+                super.addPlant(plant);
+            }
+            else {
+                // exception
+            }
+        }
+        else {
+            if (getPlant().getName() == "Lilypad") {
+                Lilypad lilypad = (Lilypad) getPlant();
+                lilypad.setPlant(plant);
+            }
+            else {
+                // exception
+            }
+        }
+    }
 }
