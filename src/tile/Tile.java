a6e855ebf53fcb6d819084818d77e977a2ff6f1f
MRaflyH <raflyhendrakusuma@gmail.com>
2024-05-08 11:09:02 +0700

    Update Tile.java

--- src/tile/Tile.java
+++ src/tile/Tile.java
@@ -1,4 +1,49 @@
 package tile;
+
+import java.util.ArrayList;
+import organism.plant.*;
+import organism.zombie.*;
+
 public abstract class Tile {
-    
+    private String name;
+    private Plant plant;
+    private ArrayList<Zombie> zombies;
+
+    public Tile() {
+        zombies = new ArrayList<Zombie>();
+    }
+
+    public void addPlant(Plant plant) {
+        if (!hasPlant()) {
+            this.plant = plant;
+        }
+        else {
+            // exception
+        }
+    }
+
+    public void removePlant() {
+        plant = null;
+    }
+
+    public void addZombie(Zombie zombie) {
+        zombies.add(zombie);
+    }
+
+    public void removeZombie(Zombie zombie) {
+        zombies.remove(zombie);
+    }
+
+    public void moveZombie(Zombie zombie, Tile tile) {
+        this.removeZombie(zombie);
+        tile.addZombie(zombie);
+    }
+
+    public boolean hasPlant() {
+        return plant != null;
+    }
+
+    public boolean hasZombie() {
+        return !zombies.isEmpty();
+    }
 }
