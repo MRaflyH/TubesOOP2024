18d2faa711ea9bf7df538a0bef0600902dbbd17e
MRaflyH <raflyhendrakusuma@gmail.com>
2024-05-08 11:21:20 +0700

    Update HomeTile.java

--- src/tile/HomeTile.java
+++ src/tile/HomeTile.java
@@ -1,5 +1,19 @@
 package tile;
 
-public class HomeTile {
-    
+import organism.plant.*;
+
+public class HomeTile extends Tile{
+
+    public HomeTile() {
+        super("Home Tile");
+    }
+
+    public void addPlant(Plant plant) {
+        // exception
+    }
+
+    public void removePlant() {
+        // exception
+    }
+
 }
