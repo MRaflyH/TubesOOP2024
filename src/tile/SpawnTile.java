949fe5ca09f11f3923737e68232f24416f217e69
MRaflyH <raflyhendrakusuma@gmail.com>
2024-05-08 11:20:25 +0700

    Update SpawnTile.java

--- src/tile/SpawnTile.java
+++ src/tile/SpawnTile.java
@@ -1,5 +1,19 @@
 package tile;
 
-public class SpawnTile {
+import organism.plant.*;
+
+public class SpawnTile extends Tile{
     
+    public SpawnTile() {
+        super("Spawn Tile");
+    }
+
+    public void addPlant(Plant plant) {
+        // exception
+    }
+
+    public void removePlant() {
+        // exception
+    }
+
 }
