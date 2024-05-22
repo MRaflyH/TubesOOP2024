8891ef3d4bfb58c30c7aed5e85c6338355092d73
MRaflyH <raflyhendrakusuma@gmail.com>
2024-05-22 09:54:26 +0700

    update Visitor

--- src/visitor/PlantVisitor.java
+++ src/visitor/PlantVisitor.java
@@ -3,6 +3,8 @@ package visitor;
 import java.util.ArrayList;
 import grid.Lawn;
 import tile.*;
+import organism.plant.*;
+import organism.zombie.*;
 
 public class PlantVisitor extends Visitor{
     private ArrayList<Tile> row;
@@ -13,9 +15,36 @@ public class PlantVisitor extends Visitor{
     }
 
     public void visit() {
-        for (Tile t : row) {
-            
+        int distance = 0;
+        Tile zombieTile = row.get(10);
+        Plant plant;
+
+        for (int i = 10; i >= 0; i--) {
+            // menyimpan tile dengan zombie terkiri
+            if (row.get(i).hasZombie()) {
+                zombieTile = row.get(i);
+                distance = 0;
+            }
+            else distance++;
+
+            // check jika ada plant
+            if (row.get(i).hasPlant()) {
+                plant = row.get(i).getPlant();
+                plant.updateState();
+
+                // check jika bisa attack
+                if (zombieTile.hasZombie() && 
+                plant.getAttackCooldown() <= 0 &&
+                (plant.getRange() == -1 || plant.getRange() >= distance)) {
+
+                    // attack dan kill zombie
+                    plant.attack();
+                    for (Zombie zombie : zombieTile.getZombies()) {
+                        zombie.loseHealth(plant.getAttackDamage());
+                        if (zombie.isDead()) zombieTile.removeZombie(zombie);
+                    }
+                }
+            }
         }
     }
-
 }
