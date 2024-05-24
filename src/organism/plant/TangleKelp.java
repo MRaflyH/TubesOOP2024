b96e81eb33c352ac1804ba90357a9690c26a2977
billysm23 <bllysm23@gmail.com>
2024-05-24 16:08:52 +0700

    tangle kelp attack, simplify zombie attack

--- src/organism/plant/TangleKelp.java
+++ src/organism/plant/TangleKelp.java
@@ -2,6 +2,6 @@ package organism.plant;
 
 public class TangleKelp extends Plant {
     public TangleKelp(){
-        super("Tangle Kelp", 100, 5000, 10, true, 25, 0, 15);
+        super("Tangle Kelp", 100, 5000, 0, true, 25, 1, 15);
     }
 }
